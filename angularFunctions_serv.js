203d1690b4039b819ec09ae7b1497965482304b7
Prubhjot Randhawa <pxr359@cs.bham.ac.uk>
2018-02-26 15:34:52 +0000

    update serv loc

--- angularFunctions_serv.js
+++ angularFunctions_serv.js
@@ -85,6 +85,8 @@ app.controller('myCtrl', function($scope, $http) {
       //  console.log(' $scope.user.id', payload.userID);
       $http.post('https://ec2-13-59-133-80.us-east-2.compute.amazonaws.com:3000/api/mail', payload).then(function(response) {
         console.log("mail sent");
+      }, function(error){
+        console.log(error);
       });
       window.alert("verification sent");
 
